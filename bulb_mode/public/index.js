68ed0f1ba3a52a716405b117c32266d25d0a7406
sumit <sumitsoni0226@gmail.com>
2021-12-30 23:13:33 +0530

    fix: minor changes

---
+++ bulb_mode/public/index.js
@@ -0,0 +1,27 @@
+document.getElementById("theme1").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--background-color",
+    "rgb(100, 63, 135)"
+  );
+});
+document.getElementById("theme2").addEventListener("click", () => {
+  document.documentElement.style.setProperty(
+    "--background-color",
+    "greenyellow"
+  );
+});
+document.getElementById("theme3").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "hotpink");
+});
+document.getElementById("theme4").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "orangered");
+});
+document.getElementById("theme5").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "teal");
+});
+document.getElementById("theme6").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "indianred");
+});
+document.getElementById("theme7").addEventListener("click", () => {
+  document.documentElement.style.setProperty("--background-color", "maroon");
+});
